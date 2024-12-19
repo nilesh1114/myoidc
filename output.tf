4ef6cb752f4eadfa699cc0a7486725289ab0712f
nilesh1114 <nileshkr1114@gmail.com>
2024-12-19 20:07:26 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "web_server_ip" {
+  value = aws_instance.web_server.public_ip
+}
